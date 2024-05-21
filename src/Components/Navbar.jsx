5d281c193209259a551bc753902c50965e02df01
evarellapucky <scherer.alexia@gmail.com>
2024-05-21 20:36:03 +0200

    logout redirects to home page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { userAtom, tokenAtom } from "../atoms/atoms"
 import { useAtom } from "jotai"
 
@@ -7,10 +7,12 @@ function Navbar() {
 
   const [user, setUser] = useAtom(userAtom)
   const [token, setToken] = useAtom(tokenAtom)
+  const redirect = useNavigate();
 
   const logOut = () => {
     setUser(null);
     setToken(null);
+    redirect("/");
   }
 
   return (
